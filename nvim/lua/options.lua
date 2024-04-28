949422626f5ea82f2557c72934b8bfcc06bed484
Danila Skripko <dalas.prog@gmail.com>
2024-04-28 12:23:14 +0400

    Add configs

---
+++ nvim/lua/options.lua
@@ -0,0 +1,11 @@
+require "nvchad.options"
+
+-- add yours here!
+
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- NEOVIDE CONFIG
+if vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h11"
+end
